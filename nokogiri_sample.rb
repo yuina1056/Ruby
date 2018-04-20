c8adddd80e1d4bc169673b0823fd7f384eadda7e
yuina <yuina.i.1056@gmail.com>
2018-04-20 14:31:02 +0900

    Update nokogiri_sample.rb

--- nokogiri_sample.rb
+++ nokogiri_sample.rb
@@ -1,12 +1,29 @@
-# Nokogiriライブラリの読み込み
-require 'nokogiri'
-require 'csv'
+require "nokogiri"
+require "csv"
 
-file = File.open('./Yahoo! JAPAN.htm')
-
-doc = Nokogiri::HTML(file)
-
-doc.css('ul#mhicon').each do |node|
-    p node.css('li').text
+def nokogiri_sample(path)
+  file = File.open(path)
+  doc = Nokogiri::HTML(file, nil, 'EN')
+  cntArray = []
+  strArray = []
+#パースしたHTMLから欲しい情報を取得して配列にぶっこむ
+  doc.css('table').each do |node|
+    node.css('tr').each do |node|
+      str1 = node.css('td[1]').text
+      str2 = node.css('td[2]').text
+      cntArray.push(str1)
+      strArray.push(str2)
+    end
   end
 
+  i = 0
+  while i < cntArray.length do
+    p cntArray[i].gsub(/[[:space:]]/," ")
+    p strArray[i].gsub(/[[:space:]]/," ")
+    puts "\n"
+    i = i + 1
+  end
+end
+
+path = 'hoge'
+nokogiri_sample(path)
